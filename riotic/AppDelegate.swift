712ed23ede53374f7609bb951c3cd1c86ce083c9
Aaron Raimist <aaron@raim.ist>
2019-02-22 21:10:43 -0600

    Enable hardened runtime and set minimum size

--- riotic/AppDelegate.swift
+++ riotic/AppDelegate.swift
@@ -12,13 +12,13 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
 
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+		
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-    }
 
+    }
 
 }
 
